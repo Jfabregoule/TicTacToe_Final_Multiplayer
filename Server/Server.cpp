92978a0c256a2e6eeb0170044d9216d950c4e9f4
jfabregoule <jfabregoule@gaming.tech>
2024-02-09 14:08:00 +0100

    Without sockets

---
+++ Server/Server.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System/Sleep.hpp>
+#include <SFML/Audio.hpp>
+
+#include "include/GameManager.h"
+
+int main()
+{
+    GameManager        game;
+
+    game.Start();
+    return 0;
+}
