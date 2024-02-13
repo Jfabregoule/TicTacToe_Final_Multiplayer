ece1937c1b14b139553e260afe652e34691e94ed
Jfabregoule <Jfabregoule@gaming.tech>
2024-02-13 19:25:40 +0100

    Thread fixed I guess

--- Client/src/Threads.cpp
+++ Client/src/Threads.cpp
@@ -1,10 +1,14 @@
+#define NOMINMAX
 #include "../include/Threads.h"
+
+#include <SFML/Graphics.hpp>
 
 Threads::Threads() : threadHandle_(NULL) {}
 
 
 
 void Threads::start() {
+    sf::Context context;
     threadHandle_ = CreateThread(NULL, 0, &Threads::threadFunctionStatic, this, 0, NULL);
     if (threadHandle_ == NULL) {
         std::cerr << "Erreur lors de la création du thread" << std::endl;
